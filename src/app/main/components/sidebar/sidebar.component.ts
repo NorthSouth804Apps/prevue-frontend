9e4069544b0100d2e3a977bea445f0661cb91343
Williams <wilfre_padilla@hotmail.com>
2022-01-26 18:40:44 -0400

    Feat: sidebar Feat: primary component 3 Feat: accordion menu Feat: logo

--- src/app/main/components/sidebar/sidebar.component.ts
+++ src/app/main/components/sidebar/sidebar.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeNode } from "primeng/api";
 
 @Component({
   selector: 'ts-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  animations: [
-  ]
+  animations: []
 })
-export class SideNavComponent implements OnInit {
-    constructor() { }
-
-    ngOnInit(): void {
+export class SidebarComponent implements OnInit {
+  display: boolean = true;
+  sidebarOptions: TreeNode[] = [
+    {
+      label: 'home',
+    },
+    {
+      label: 'analytics',
+      expandedIcon: true,
+      children: [
+        {
+          label: 'match data',
+        },
+        {
+          label: 'user data',
+        },
+      ],
+    },
+    {
+      label: 'user',
+    },
+    {
+      label: 'reported',
+    },
+    {
+      label: 'log out'
     }
+  ]
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './containers/main/main.component';
 import { MainRoutingModule } from './main-routing.module';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import {TreeModule} from 'primeng/tree';
+import {SidebarModule} from 'primeng/sidebar';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, SidebarComponent],
   imports: [
     MainRoutingModule,
+    SidebarModule,
+    TreeModule,
+    ButtonModule,
   ],
   providers: []
 })
 export class MainModule {
 }
+
